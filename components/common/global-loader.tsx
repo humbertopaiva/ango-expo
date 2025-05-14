f49bccdebb5fd3c594cdc1107a9f718907f20a48
Humberto Paiva <humbertomoreira93@gmail.com>
2025-05-14 10:55:17 -0300

    fix: remove shadoOffset from reorder component

--- components/common/global-loader.tsx
+++ components/common/global-loader.tsx
@@ -59,7 +59,6 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
--- components/common/navigation-loader.tsx
+++ components/common/navigation-loader.tsx
@@ -35,7 +35,6 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
